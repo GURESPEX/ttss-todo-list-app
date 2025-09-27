27e53d26a88ba58e8bb8e88f18d5ed37c0fa9cd1
Natthanan <natthanan.th@techconsbiz.com>
2025-09-28 02:49:13 +0700

    [Update]: Change JWT algorithm to HS256

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -3,5 +3,5 @@ import { SignatureAlgorithm } from "hono/utils/jwt/jwa";
 export default {
   ACCESS_TOKEN_SECRET: "access-token-secret" as string,
   REFRESH_TOKEN_SECRET: "refresh-token-secret" as string,
-  JWT_ALGORITHM: "RS256" as SignatureAlgorithm,
+  JWT_ALGORITHM: "HS256" as SignatureAlgorithm,
 } as const;
