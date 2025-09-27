938a801edb4ee240046ac0d5f8ebd7b4cac62de3
Natthanan <natthanan.th@techconsbiz.com>
2025-09-28 02:50:01 +0700

    [Update]: Fix Reqponse utility baseBodyObject not return result field

--- backend/src/utils/response/index.ts
+++ backend/src/utils/response/index.ts
@@ -1,7 +1,17 @@
 import { StatusCode } from "hono/utils/http-status";
 
 export abstract class Response {
-  public static baseBodyObject = <T>(body: ResponseBaseParameter<T>) => {
+  public static baseBodyObject = <T>(
+    body: ResponseBaseParameter<T>
+  ): ResponseBaseReturn<T> => {
+    if (body.success) {
+      return {
+        success: body.success,
+        result: body.result,
+        message: body.message,
+        status: body.status,
+      };
+    }
     return {
       success: body.success,
       message: body.message,
@@ -11,6 +21,19 @@ export abstract class Response {
 }
 
 type ResponseBaseParameter<T> =
+  | {
+      success: true;
+      result: T;
+      message: string;
+      status: StatusCode | (number & {});
+    }
+  | {
+      success: false;
+      message: string;
+      status: StatusCode | (number & {});
+    };
+
+type ResponseBaseReturn<T> =
   | {
       success: true;
       result: T;
