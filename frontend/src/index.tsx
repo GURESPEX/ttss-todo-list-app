390d6a9b4ddf39967898d6caa9fa34d395ba6d04
Natthanan <natthanan.th@techconsbiz.com>
2025-09-29 22:58:42 +0700

    [Remove]: Remove react router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
 import App from "./app";
 import "./index.css";
 import { ApiProvider } from "@reduxjs/toolkit/query/react";
@@ -9,9 +8,7 @@ import api from "./api";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ApiProvider api={api}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </ApiProvider>
   </StrictMode>
 );
