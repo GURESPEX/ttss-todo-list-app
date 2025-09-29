cb57ff233c0b58c97b89f9802af55dfbe5e31be1
Natthanan <natthanan.th@techconsbiz.com>
2025-09-29 16:05:45 +0700

    [Update]: Setup new Frontend (Clean and Remove files)

--- frontend/src/env.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  IS_DEVELOPMENT_MODE: import.meta.env.DEV,
-  IS_PRODUCTION_MODE: import.meta.env.PROD,
-  MODE: import.meta.env.MODE as "development" | "production",
-  API_URL: "http://localhost:5001/api",
-} as const;
--- frontend/src/utils/helper/cn.ts
+++
@@ -1,6 +0,0 @@
-import clsx, { type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export default (...inputs: ClassValue[]) => {
-  return twMerge(clsx(inputs));
-};
