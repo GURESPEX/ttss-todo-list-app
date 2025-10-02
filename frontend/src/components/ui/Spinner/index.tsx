e114fc489a72388765faea22aeb44e16fd89d073
Natthanan <natthanan.th@techconsbiz.com>
2025-10-03 00:30:32 +0700

    [Add]: Add color prop for Spinner component

--- frontend/src/components/ui/Spinner/index.tsx
+++ frontend/src/components/ui/Spinner/index.tsx
@@ -1,8 +1,18 @@
+import React from "react";
 import { cn } from "../../../utils";
 import type { SpinnerProps } from "./type";
+import type { ClassValue } from "clsx";
 
-const Spinner = ({ className, ...props }: SpinnerProps) => {
-  return <div className={cn(className, "aspect-square animate-spin rounded-xs bg-white", "w-6", props.size === "small" && "w-3", props.size === "large" && "w-8")} />;
+const Spinner = ({ color = "default", className, ...props }: SpinnerProps) => {
+  return <div className={cn("aspect-square animate-spin rounded-xs", "w-6", props.size === "small" && "w-3", props.size === "large" && "w-8", spinnerTailwindColorMap[color], className)} />;
 };
 
-export default Spinner;
+const spinnerTailwindColorMap: Record<Exclude<SpinnerProps["color"], undefined>, ClassValue> = {
+  primary: "bg-blue-500",
+  success: "bg-green-500",
+  warning: "bg-amber-500",
+  danger: "bg-red-500",
+  default: "bg-white",
+};
+
+export default React.memo(Spinner);
--- frontend/src/components/ui/Spinner/type.ts
+++ frontend/src/components/ui/Spinner/type.ts
@@ -1,3 +1,3 @@
 import type { ComponentProps } from "react";
 
-export type SpinnerProps = ComponentProps<"div"> & { size?: "small" | "medium" | "large" };
+export type SpinnerProps = ComponentProps<"div"> & { color?: "primary" | "danger" | "warning" | "success" | "default"; size?: "small" | "medium" | "large" };
