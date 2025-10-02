4046dc5da8e5bc9e340232ac9cfe630426ec248a
Natthanan <natthanan.th@techconsbiz.com>
2025-10-02 23:55:19 +0700

    [Add]: Add result type of user and todo

--- frontend/src/api/todo/type.ts
+++ frontend/src/api/todo/type.ts
@@ -1,9 +1,18 @@
 import type { EndpointType, ReponseResult } from "../type";
 
 export type TodoApi = {
-  GetAll: EndpointType<{ access_token: string }, unknown, ReponseResult<unknown>>;
-  GetById: EndpointType<{ access_token: string; id: string }, unknown, ReponseResult<unknown>>;
-  Create: EndpointType<{ access_token: string; title?: string; content: string }, unknown, ReponseResult<unknown>>;
-  Update: EndpointType<{ access_token: string; id: string; title?: string; content: string; is_done: string }, unknown, ReponseResult<unknown>>;
-  Delete: EndpointType<{ access_token: string; id: string }, unknown, ReponseResult<unknown>>;
+  GetAll: EndpointType<{ access_token: string }, unknown, ReponseResult<Todo[]>>;
+  GetById: EndpointType<{ access_token: string; id: string }, unknown, ReponseResult<Todo>>;
+  Create: EndpointType<{ access_token: string; title?: string; content: string }, unknown, ReponseResult<Todo>>;
+  Update: EndpointType<{ access_token: string; id: string; title?: string; content: string; is_done: boolean }, unknown, ReponseResult<Todo>>;
+  Delete: EndpointType<{ access_token: string; id: string }, unknown, ReponseResult<Todo>>;
+};
+
+export type Todo = {
+  id: string;
+  title: string;
+  content: string;
+  is_done: boolean;
+  created_at: string;
+  updated_at: string;
 };
--- frontend/src/api/user/type.ts
+++ frontend/src/api/user/type.ts
@@ -1,7 +1,9 @@
 import type { EndpointType, ReponseResult } from "../type";
 
 export type UserApi = {
-  GetMe: EndpointType<{ access_token: string }, unknown, ReponseResult<{ id: string; username: string; created_at: string; updated_at: string }>>;
-  UpdateMe: EndpointType<{ access_token: string; username: string; password: string }, unknown, ReponseResult<{ id: string; username: string; created_at: string; updated_at: string }>>;
-  DeleteMe: EndpointType<{ access_token: string }, unknown, ReponseResult<{ id: string; username: string; created_at: string; updated_at: string }>>;
+  GetMe: EndpointType<{ access_token: string }, unknown, ReponseResult<User>>;
+  UpdateMe: EndpointType<{ access_token: string; username: string; password: string }, unknown, ReponseResult<User>>;
+  DeleteMe: EndpointType<{ access_token: string }, unknown, ReponseResult<User>>;
 };
+
+export type User = { id: string; username: string; created_at: string; updated_at: string };
