86271b135ec52136ba9ed1b57867f27359028aa8
Natthanan <natthanan.th@techconsbiz.com>
2025-10-03 00:35:01 +0700

    [Update]: Change file location to components directory

---
+++ frontend/src/components/LoginForm/index.tsx
@@ -0,0 +1,76 @@
+import React, { useCallback, useState } from "react";
+import Button from "../ui/Button";
+import Checkbox from "../ui/Checkbox";
+import Input from "../ui/Input";
+import type { CheckboxProps } from "../ui/Checkbox/type";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useAuth } from "../../contexts/AuthProvider/hooks";
+import Label from "../ui/Label";
+
+const loginFieldValuesSchema = z.object({
+  username: z
+    .string()
+    .min(4)
+    .max(32)
+    .regex(/^[A-z0-9_.]+$/),
+  password: z.string().min(8),
+});
+
+type LoginFieldValues = z.infer<typeof loginFieldValuesSchema>;
+
+const LoginForm = () => {
+  const [isShowPassword, setIsShowPassword] = useState<boolean>(false);
+
+  const { loginHandler } = useAuth();
+
+  const { register, handleSubmit, formState, reset, setError } = useForm<LoginFieldValues>({ resolver: zodResolver(loginFieldValuesSchema) });
+
+  const onSubmit = useCallback<SubmitHandler<LoginFieldValues>>(
+    async (data) => {
+      const error = await loginHandler.login({ username: data.username, password: data.password });
+      if (error) {
+        setError("root", { message: error.error.data.message });
+        return;
+      }
+      reset();
+    },
+    [loginHandler, reset, setError],
+  );
+
+  const handleChangeIsShowPassword = useCallback<Exclude<CheckboxProps["onChange"], undefined>>((event) => {
+    setIsShowPassword(event.target.checked);
+  }, []);
+
+  return (
+    <form className="flex w-full flex-col gap-2 p-4" onSubmit={handleSubmit(onSubmit)}>
+      <h2 className="text-2xl font-bold">Login</h2>
+      <Label label="Username">
+        {(id) => (
+          <>
+            <Input type="text" id={id} placeholder="Username" {...register("username")} />
+            <div className="text-sm text-red-500">{formState.errors.username?.message}</div>
+          </>
+        )}
+      </Label>
+      <Label label="Password">
+        {(id) => (
+          <>
+            <Input type={isShowPassword ? "text" : "password"} id={id} placeholder="Password" {...register("password")} />
+            <div className="text-sm text-red-500">{formState.errors.password?.message}</div>
+          </>
+        )}
+      </Label>
+      <Checkbox checked={isShowPassword} onChange={handleChangeIsShowPassword}>
+        Show password
+      </Checkbox>
+
+      <div className="text-sm text-red-500">{formState.errors.root?.message}</div>
+
+      <Button loading={loginHandler.loginState.isLoading}>Login</Button>
+    </form>
+  );
+};
+
+export default React.memo(LoginForm);
---
+++ frontend/src/components/RegisterForm/index.tsx
@@ -0,0 +1,93 @@
+import React, { useCallback, useState } from "react";
+import Button from "../ui/Button";
+import Checkbox from "../ui/Checkbox";
+import Input from "../ui/Input";
+import type { CheckboxProps } from "../ui/Checkbox/type";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { authApi } from "../../api/auth";
+import Label from "../ui/Label";
+
+const registerFieldValuesSchema = z.object({
+  username: z
+    .string()
+    .min(4)
+    .max(32)
+    .regex(/^[A-z0-9_.]+$/),
+  password: z.string().min(8),
+  confirmPassword: z.string().min(8),
+});
+
+type RegisterFieldValues = z.infer<typeof registerFieldValuesSchema>;
+
+const RegisterForm = () => {
+  const [isShowPassword, setIsShowPassword] = useState<boolean>(false);
+
+  const [callRegister, { isLoading }] = authApi.useRegisterMutation();
+
+  const { register, handleSubmit, formState, reset, setError } = useForm<RegisterFieldValues>({ resolver: zodResolver(registerFieldValuesSchema) });
+
+  const onSubmit = useCallback<SubmitHandler<RegisterFieldValues>>(
+    async (data) => {
+      const { error } = await callRegister({
+        username: data.username,
+        password: data.password,
+      });
+      if (error) {
+        setError("root", { message: error.data.message });
+        return;
+      }
+      reset();
+    },
+    [callRegister, reset, setError],
+  );
+
+  const handleChangeIsShowPassword = useCallback<Exclude<CheckboxProps["onChange"], undefined>>((event) => {
+    setIsShowPassword(event.target.checked);
+  }, []);
+
+  return (
+    <form className="flex w-full flex-col gap-2 p-4" onSubmit={handleSubmit(onSubmit)}>
+      <h2 className="text-2xl font-bold">Register</h2>
+      <form action=""></form>
+      <Label label="Username">
+        {(id) => (
+          <>
+            <Input type="text" id={id} placeholder="Username" {...register("username")} />
+            <div className="text-sm text-red-500">{formState.errors.username?.message}</div>
+          </>
+        )}
+      </Label>
+      <div className="flex gap-2">
+        <Label label="Password">
+          {(id) => (
+            <>
+              <Input type={isShowPassword ? "text" : "password"} id={id} placeholder="Password" {...register("password")} />
+              <div className="text-sm text-red-500">{formState.errors.password?.message}</div>
+            </>
+          )}
+        </Label>
+        <Label label="Confirm password">
+          {(id) => (
+            <>
+              <Input type={isShowPassword ? "text" : "password"} id={id} placeholder="Confirm password" {...register("confirmPassword")} />
+              <div className="text-sm text-red-500">{formState.errors.confirmPassword?.message}</div>
+            </>
+          )}
+        </Label>
+      </div>
+
+      <div className="text-sm text-red-500">{formState.errors.root?.message}</div>
+
+      <Checkbox checked={isShowPassword} onChange={handleChangeIsShowPassword}>
+        Show password
+      </Checkbox>
+      <Button type="submit" loading={isLoading}>
+        Register
+      </Button>
+    </form>
+  );
+};
+
+export default React.memo(RegisterForm);
